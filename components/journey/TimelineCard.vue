bb2fa03537161ffea68b8ef24a17336a2eefd035
EyobAshenaki <ethioeyoba@gmail.com>
2023-01-23 15:14:26 +0300

    feat(journey): add journey page

---
+++ components/journey/TimelineCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="w-[34rem] flex justify-between items-center -translate-y-1/2 shadow-lg rounded-2xl border border-brand-text px-12 py-10"
+    :class="{
+      '-translate-x-full': isIdEven,
+    }"
+  >
+    <p>
+      <span class="font-semibold text-lg text-brand-text">“</span>
+      {{ item.quote }}
+      <span class="font-semibold text-lg text-brand-text">”</span>
+    </p>
+    <img :src="getImage" alt="Timeline Image" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TimelineCard',
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    isIdEven() {
+      return this.item.id % 2 === 0
+    },
+
+    getImage() {
+      return require(`~/assets/images/timeline_images/${this.item.image}`)
+    },
+  },
+}
+</script>
--- pages/journey.vue
+++ pages/journey.vue
@@ -1,11 +1,136 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">Journey!</h1>
-  </div>
+  <section class="flex flex-col items-center gap-7">
+    <header class="flex flex-col items-center mt-14">
+      <h2 class="font-medium text-lg">Journey of</h2>
+      <h1 class="font-bold text-xl md:text-2xl text-brand-text">
+        Micheal Testa
+      </h1>
+    </header>
+    <div class="md:w-[75%] flex flex-col gap-5 text-center">
+      <p>
+        It is estimated that by the 2030s, 9 out 10 people would need to
+        retrain. I've been constantly retraining over the last two decades as
+        outlined below. I started my career as an entrepreneur trading second
+        hand video games. I then pursued this until I got to university where I
+        retrained as an Economist.
+      </p>
+      <p>
+        On this path, I joined the Ministry of Justice writing Business Cases
+        and producing Cost Benefit Models. At the Ministry of Justice, there was
+        a shortage of Data Scientists and so I volunteered and started
+        retraining, I managed to produce several R Shiny Dashboards, one of
+        which was a tool that embedded a discrete event simulation leading to
+        massive spending decisions and the creation of the Independent
+        Monitoring Authority, a new organization set up to support EU Citizen’s
+        Rights. With my new found love for code and data and my old age passion
+        for Climate Change Mitigation, I decided to join BEIS and to led the
+        largest household energy dataset in the UK, The National Energy
+        Efficiency Data Framework.
+      </p>
+      <p>
+        However, Data Science wasn't enough, I had to retrain as a Team Manager
+        and as a Classical Statistician. During this time, I caught the
+        Entrepreneurial bug again and decided to experiment with app building.
+        So I started to retrain as an MVP builder. I fell in love with Climate
+        Tech and so I then decided to retrain as an Environmental Economist by
+        getting a job as the Head of Carbon Pricing Strategy.
+      </p>
+      <p>
+        I managed to excel and got invited to speak at The Economist’s US
+        Sustainability Week where I spoke about Carbon as an Asset Class.
+      </p>
+      <p>
+        Fast forward to today, I'm retraining yet again in Capital Markets at
+        Goldman Sachs.
+      </p>
+    </div>
+
+    <!-- Timeline -->
+    <div class="flex justify-center my-36">
+      <ol
+        class="timeline relative translate-x-1/2 border-l-2 border-brand-text"
+      >
+        <li
+          v-for="timelineItem in timelineItems"
+          :key="timelineItem.id"
+          class="mb-10"
+          :class="{
+            'ml-6': !isIdEven(timelineItem.id),
+            '-ml-6': isIdEven(timelineItem.id),
+            '-mb-[35%]': lastTimelineItemIndex === timelineItem.id,
+          }"
+        >
+          <div
+            class="absolute -left-[7px] w-3 h-3 bg-brand-text rounded-full"
+          ></div>
+          <timeline-card :item="timelineItem" />
+        </li>
+      </ol>
+    </div>
+  </section>
 </template>
 
 <script>
+import TimelineCard from '~/components/journey/TimelineCard.vue'
 export default {
   name: 'Journey',
+  components: {
+    TimelineCard,
+  },
+
+  data() {
+    return {
+      timelineItems: [
+        {
+          id: 1,
+          quote:
+            'I began my first business at the age of eleven by renting out my bicycle and exchanging used video games.',
+          image: 'timeline_1.png',
+        },
+        {
+          id: 2,
+          quote:
+            "In a business pitch competition, I came in second place at age 17, and at age 20, I got a scholarship to study for a master's degree at UCL.",
+          image: 'timeline_2.png',
+        },
+        {
+          id: 3,
+          quote:
+            'At the age of 23, I assisted in the development of a new Quango by building a simulation model. I am appointed team leader of the National Energy Efficiency Data Team at the age of 25.',
+          image: 'timeline_3.png',
+        },
+        {
+          id: 4,
+          quote:
+            "I released Key Data and Statistics on Energy Efficiency when I was 26 years old and was a member of the Eco-Innovation Board. A year later, I made a significant strategic contribution to a report on the UK's future carbon markets.",
+          image: 'timeline_4.png',
+        },
+        {
+          id: 5,
+          quote:
+            "At the age of 27, I gave a speech at The Economist's US Sustainability Week, which attracted over 10,000 people. I also received a spot at UCL's Hatchery, a competitive incubator.",
+          image: 'timeline_5.png',
+        },
+        {
+          id: 6,
+          quote:
+            'I have began my new position at Goldman Sachs as Head of Carbon and Environmental Product Strategy just before turning 28. I would like to subscribe 2,000 households for GreenCheddar.',
+          image: 'timeline_6.png',
+        },
+      ],
+    }
+  },
+
+  computed: {
+    lastTimelineItemIndex() {
+      return this.timelineItems.length
+    },
+  },
+
+  methods: {
+    isIdEven(val) {
+      return val % 2 === 0
+    },
+  },
 }
 </script>
