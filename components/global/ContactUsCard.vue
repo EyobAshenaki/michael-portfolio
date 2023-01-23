0f7aa3b7673c9b2376e60e90c8389234c611961c
EyobAshenaki <ethioeyoba@gmail.com>
2023-01-23 15:28:36 +0300

    feat: add contact us card to footer
    - Added contact us card to footer when not on the home page
    - Removed twitter and instagram icon from contact us card

--- components/global/ContactUsCard.vue
+++ components/global/ContactUsCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-fit relative flex flex-col gap-2.5 rounded-3xl bg-accent drop-shadow-[0_8px_18px_#a5733847] p-8 md:p-11 py-6"
+    class="w-fit h-fit relative flex flex-col gap-2.5 rounded-3xl bg-accent drop-shadow-[0_8px_18px_#a5733847] p-8 md:p-11 py-6"
   >
     <falling-star-icon class="absolute top-0 right-0" />
     <header class="font-bold text-lg text-center pb-1">Contact me</header>
@@ -15,8 +15,6 @@
       <h3 class="font-semibold">Social Medias</h3>
       <div class="flex gap-5">
         <linkedin-icon />
-        <twitter-icon />
-        <instagram-icon />
       </div>
     </div>
   </div>
@@ -25,16 +23,12 @@
 <script>
 import EmailIcon from '~/components/icons/social/EmailIcon.vue'
 import LinkedinIcon from '~/components/icons/social/LinkedinIcon.vue'
-import TwitterIcon from '~/components/icons/social/TwitterIcon.vue'
-import InstagramIcon from '~/components/icons/social/InstagramIcon.vue'
 import FallingStarIcon from '~/components/icons/FallingStarIcon.vue'
 export default {
   name: 'ContactUsCard',
   components: {
     EmailIcon,
     LinkedinIcon,
-    TwitterIcon,
-    InstagramIcon,
     FallingStarIcon,
   },
 }
--- components/layout/FooterSection.vue
+++ components/layout/FooterSection.vue
@@ -1,9 +1,18 @@
 <template>
-  <footer class="h-24 bg-brand"></footer>
+  <footer class="h-24 flex justify-end bg-brand">
+    <contact-us-card class="-mt-[15%] mr-[5%]" :class="{ hidden: isHome }" />
+  </footer>
 </template>
 
 <script>
+import ContactUsCard from '~/components/global/ContactUsCard.vue'
 export default {
   name: 'FooterSection',
+  components: { ContactUsCard },
+  computed: {
+    isHome() {
+      return this.$route.path === '/'
+    },
+  },
 }
 </script>
