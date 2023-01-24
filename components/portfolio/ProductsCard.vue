c11ed2e89e7d395364dce311435d068a9b4c766e
EyobAshenaki <ethioeyoba@gmail.com>
2023-01-24 07:40:37 +0300

    feat: add products page with responsiveness

---
+++ components/portfolio/ProductsCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="flex flex-col lg:flex-row justify-between gap-4 lg:gap-0">
+    <div class="basis-[60%] flex flex-col gap-7">
+      <h3 class="font-bold">{{ item.title }}</h3>
+      <p>
+        {{ item.description }}
+      </p>
+
+      <div class="flex items-center gap-1">
+        <a
+          class="font-semibold text-lg text-green-600"
+          :href="item.link"
+          target="”_blank”"
+        >
+          {{ item.linkText }}
+        </a>
+        <link-arrow-icon />
+      </div>
+    </div>
+    <div class="basis-[30%] relative flex justify-end">
+      <img
+        class="absolute top-24 right-24 md:right-36 rounded-2xl"
+        :src="getImageFront"
+        alt="Products Front Image"
+      />
+      <img
+        class="rounded-2xl"
+        :src="getImageBehind"
+        alt="Products Behind Image"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import LinkArrowIcon from '~/components/icons/LinkArrowIcon.vue'
+
+export default {
+  name: 'ProductsCard',
+
+  components: {
+    LinkArrowIcon,
+  },
+
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    getImageFront() {
+      return require(`~/assets/images/recent_works/products/${this.item.imageFront}`)
+    },
+    getImageBehind() {
+      return require(`~/assets/images/recent_works/products/${this.item.imageBehind}`)
+    },
+  },
+}
+</script>
--- pages/portfolio/products.vue
+++ pages/portfolio/products.vue
@@ -1,11 +1,63 @@
 <template>
-  <div>
-    <h1>Products page</h1>
-  </div>
+  <section class="flex flex-col mb-56">
+    <header class="font-bold text-3xl text-brand-text mb-6">Products</header>
+    <div class="flex flex-col gap-14">
+      <p>
+        Please see below products that are ready for use today. We are cost
+        effective and reliable. Thank you in advance for your interest.
+      </p>
+      <div class="flex flex-col gap-32 mb-16">
+        <products-card
+          v-for="(product, idx) in products"
+          :key="idx"
+          :item="product"
+        />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import ProductsCard from '~/components/portfolio/ProductsCard.vue'
 export default {
   name: 'ProductsPage',
+
+  components: { ProductsCard },
+
+  layout: 'portfolio_layout',
+
+  data() {
+    return {
+      products: [
+        {
+          title: 'Greencheddar',
+          description:
+            'GreenCheddar is a sustainable climate-tech app that provides employees incentives to turn up to the office while also helping them in lowering their energy bills. Employees receive tailored energy infographics while enhancing their engagement levels at work.',
+          link: 'greencheddar.earth/business',
+          linkText: 'Greencheddar',
+          imageFront: 'products_image_1.png',
+          imageBehind: 'products_image_2.png',
+        },
+        {
+          title: 'Carbon Miner',
+          description:
+            'You can do these calculations yourself but why bother when you have it all in one place. This website is for people who want to understand the environmental impact of buying large quantities of commodities.',
+          link: 'greencheddar.earth/business',
+          linkText: 'Carbon Miner',
+          imageFront: 'products_image_3.png',
+          imageBehind: 'products_image_4.png',
+        },
+        {
+          title: 'Green Menu',
+          description:
+            "If you're a restaurants looking to understand the Carbon footprint of your menu, please check out our free calculator. We also offer design services if you're looking to revamp your Menu with our designs. Communicating the Carbon footprint to your customers might help them manage their life's easier for example if they have had one of your tasty burgers, maybe they can offset that my buying credits or taking public transportation instead of an Uber ride back home.",
+          link: 'greencheddar.earth/business',
+          linkText: 'Green Menu',
+          imageFront: 'products_image_5.png',
+          imageBehind: 'products_image_6.png',
+        },
+      ],
+    }
+  },
 }
 </script>
