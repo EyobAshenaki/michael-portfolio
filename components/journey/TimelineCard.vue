8b71cd4ee948fbc86b725af8d41dc598317dce10
EyobAshenaki <ethioeyoba@gmail.com>
2023-01-25 15:42:17 +0300

    style: add animation on the timeline cards

--- components/journey/TimelineCard.vue
+++ components/journey/TimelineCard.vue
@@ -1,27 +1,45 @@
 <template>
   <div
-    class="md:w-80 lg:w-[34rem] flex flex-col sm:flex-row md:flex-col lg:flex-row justify-between items-center -translate-y-1/2 shadow-lg rounded-2xl border border-brand-text px-6 lg:px-12 py-5 lg:py-10"
+    class="md:w-80 lg:w-[34rem] h-96 sm:h-60 md:h-auto flex flex-col sm:flex-row md:flex-col lg:flex-row justify-between items-center -translate-y-1/2 shadow-lg rounded-2xl border border-brand-text px-6 lg:px-12 py-5 lg:py-10"
     :class="{
       'md:-translate-x-full': isIdEven,
     }"
   >
-    <p>
+    <p class="basis-2/3">
       <span class="font-semibold text-lg text-brand-text">“</span>
       {{ item.quote }}
       <span class="font-semibold text-lg text-brand-text">”</span>
     </p>
-    <img :src="getImage" alt="Timeline Image" />
+    <lottie
+      class="sm:basis-1/3"
+      :options="lottieOptions"
+      @animCreated="handleAnimation"
+    />
   </div>
 </template>
 
 <script>
+import Lottie from 'vue-lottie/src/lottie.vue'
+
 export default {
   name: 'TimelineCard',
+
+  components: {
+    Lottie,
+  },
+
   props: {
     item: {
       type: Object,
       required: true,
     },
+  },
+
+  data() {
+    return {
+      anim: null, // for saving the reference to the animation
+      lottieOptions: { animationData: this.item.animationData.default },
+    }
   },
 
   computed: {
@@ -31,6 +49,12 @@ export default {
 
     getImage() {
       return require(`~/assets/images/timeline_images/${this.item.image}`)
+    },
+  },
+
+  methods: {
+    handleAnimation: function (anim) {
+      this.anim = anim
     },
   },
 }
--- pages/journey.vue
+++ pages/journey.vue
@@ -46,9 +46,9 @@
     </div>
 
     <!-- Timeline -->
-    <div class="flex justify-center my-44 md:my-36">
+    <div class="relative flex justify-center my-44 md:my-36">
       <ol
-        class="timeline relative md:translate-x-1/2 border-l-2 border-brand-text"
+        class="timeline relative md:translate-x-1/2 border-l-2 border-brand-text z-[1]"
       >
         <li
           v-for="timelineItem in timelineItems"
@@ -57,14 +57,14 @@
           :class="{
             'ml-6': !isIdEven(timelineItem.id),
             'ml-6 md:-ml-6': isIdEven(timelineItem.id),
-            '-mb-[23rem] sm:-mb-52 md:-mb-[23rem] lg:-mb-64':
+            '-mb-[24rem] sm:-mb-60 md:-mb-[24rem] lg:-mb-56':
               lastTimelineItemIndex === timelineItem.id,
           }"
         >
           <div
             class="absolute -left-[7px] w-3 h-3 bg-brand-text rounded-full"
           ></div>
-          <timeline-card :item="timelineItem" />
+          <timeline-card class="bg-white" :item="timelineItem" />
         </li>
       </ol>
     </div>
@@ -73,6 +73,12 @@
 
 <script>
 import TimelineCard from '~/components/journey/TimelineCard.vue'
+import * as timelineData1 from '~/assets/animations/timeline_data_1.json'
+import * as timelineData2 from '~/assets/animations/timeline_data_2.json'
+import * as timelineData3 from '~/assets/animations/timeline_data_3.json'
+import * as timelineData4 from '~/assets/animations/timeline_data_4.json'
+import * as timelineData5 from '~/assets/animations/timeline_data_5.json'
+import * as timelineData6 from '~/assets/animations/timeline_data_6.json'
 export default {
   name: 'Journey',
   components: {
@@ -86,37 +92,37 @@ export default {
           id: 1,
           quote:
             'I began my first business at the age of eleven by renting out my bicycle and exchanging used video games.',
-          image: 'timeline_1.png',
+          animationData: timelineData1,
         },
         {
           id: 2,
           quote:
             "In a business pitch competition, I came in second place at age 17, and at age 20, I got a scholarship to study for a master's degree at UCL.",
-          image: 'timeline_2.png',
+          animationData: timelineData2,
         },
         {
           id: 3,
           quote:
             'At the age of 23, I assisted in the development of a new Quango by building a simulation model. I am appointed team leader of the National Energy Efficiency Data Team at the age of 25.',
-          image: 'timeline_3.png',
+          animationData: timelineData3,
         },
         {
           id: 4,
           quote:
             "I released Key Data and Statistics on Energy Efficiency when I was 26 years old and was a member of the Eco-Innovation Board. A year later, I made a significant strategic contribution to a report on the UK's future carbon markets.",
-          image: 'timeline_4.png',
+          animationData: timelineData4,
         },
         {
           id: 5,
           quote:
             "At the age of 27, I gave a speech at The Economist's US Sustainability Week, which attracted over 10,000 people. I also received a spot at UCL's Hatchery, a competitive incubator.",
-          image: 'timeline_5.png',
+          animationData: timelineData5,
         },
         {
           id: 6,
           quote:
             'I have began my new position at Goldman Sachs as Head of Carbon and Environmental Product Strategy just before turning 28. I would like to subscribe 2,000 households for GreenCheddar.',
-          image: 'timeline_6.png',
+          animationData: timelineData6,
         },
       ],
     }
@@ -127,6 +133,16 @@ export default {
       return this.timelineItems.length
     },
   },
+
+  // mounted() {
+  // this.$lottie.loadAnimation({
+  //   container: document.getElementById('animation-lottie'),
+  //   renderer: 'svg',
+  //   loop: true,
+  //   autoplay: true,
+  //   path: '/lottie/animation.json',
+  // })
+  // },
 
   methods: {
     isIdEven(val) {
